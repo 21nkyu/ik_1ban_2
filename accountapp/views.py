ea0e509a5efafd26c348f27989d468538371db99
21nkyu <adele7178@naver.com>
2021-07-31 17:48:20 +0900

    20210731_scd_003

--- accountapp/views.py
+++ accountapp/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 
 
 def hello_1 (request):
-    return render(request, 'accountapp/overpower.html')
+    if request.method == 'POST':
+        return render(request, 'accountapp/overpower.html', context={'text':'POST'})
+    else:
+        return render(request, 'accountapp/overpower.html', context={'text': 'GET'})
