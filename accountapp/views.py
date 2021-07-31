a5ca9de2b66eae0a593913c29294118b4eae1e4b
21nkyu <adele7178@naver.com>
2021-07-31 16:03:39 +0900

    20210731_scd_002

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def hello_1(request):
-    return HttpResponse('hello world!')
+def hello_1 (request):
+    return render(request, 'accountapp/overpower.html')
