22db5a12a391997cfe2213608e7c7c4c18792fb6
21nkyu <adele7178@naver.com>
2021-08-01 20:29:49 +0900

    20210801_scd_010

--- profileapp/urls.py
+++ profileapp/urls.py
@@ -1,6 +1,12 @@
+from django.urls import path
+
+from profileapp.views import ProfileCreateView
 
 app_name = 'profileapp'
 
 urlpatterns = [
+    path('create/', ProfileCreateView.as_view(), name='create')
 
 ]
+
+# (경로/ - 로직 -라우팅이름)
