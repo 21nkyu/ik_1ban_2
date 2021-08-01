f5d09200ba2f5f390e6e509a6c6cf9601da44c74
21nkyu <adele7178@naver.com>
2021-08-01 16:21:49 +0900

    20210801_scd_005

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,11 +1,17 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import hello_1
+from accountapp.views import hello_1, AccountCreateview
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('overpower/', hello_1, name='hello_world')
+    path('overpower/', hello_1, name='hello_world'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
+    path('create/', AccountCreateview.as_view(), name='create'),
 
 ]
 
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import accapp_HelloWorld
 
@@ -19,3 +22,9 @@ def hello_1(request):
     else:
         hello_1_list = accapp_HelloWorld.objects.all()
         return render(request, 'accountapp/overpower.html', context={'hello_1_list': hello_1_list})
+
+class AccountCreateview(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
