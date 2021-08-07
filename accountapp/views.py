24bff4dbd18e168e189be938e46b74dabe2df656
21nkyu <adele7178@naver.com>
2021-08-08 00:10:34 +0900

    20210807_scd_010(022) html minor change001

--- accountapp/views.py
+++ accountapp/views.py
@@ -57,7 +57,7 @@ class AccountUpdateView(UpdateView):
     template_name = 'accountapp/update.html'
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
+        return reverse('accountapp:detail', kwargs={'pk': self.object.pk})
 
 @method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
